7875876bc19e2cf13e5cfba6311abe0b065613eb
Thomas Nickel <tanickel1@gmail.com>
2023-02-10 21:49:27 -0500

    significantly rewrote code, with help of online tutorial, properlly formatiing logic

--- app.js
+++ app.js
@@ -1,13 +1,68 @@
-//for the user choices, I originally tried to make them each seperate variable. 
-//After consulting a more experienced developer, it made more sense to make them a single array. 
+const choices = ["Rock", "Paper", "Scissors"];
 
-const userChoices = ["rock", "paper", "scissors"]; 
-
-
-let userSelection = prompt("Want to play a game? Rock, Paper or Scissors?");
-
-if(userSelection === "rock" || userSelection === "paper" || userSelection === "scissors") {
-    console.log(userSelection)
-} else {
-    console.log("Invalid choice, try again.");
+let playerScore = 0; 
+let computerScore = 0;
+let playerSelection = " ";
+let  roundWinner = " ";
+ 
+function game(){
+    for(let i = 0, i <= 5, i++){
+        round()
+    }
 }
+
+function playerChoice () {
+    let playerSelection = prompt("Lets play a game. Rock, Paper, or Scissors?");
+    while (playerSelection = null) {
+        playerSelection = prompt("Lets play a game. Rock, Paper, or Scissors?")
+    }
+    playerSelection = playerSelection.toLowerCase();
+    let check = validateInput(playerSelection);
+    while (check == false){
+        prompt(
+            "Please select a valid choice. Spelling needs to be correct."
+            );
+    input = input.toLowerCase();
+    check = validateInput(input);
+    }
+  return playerSelection; 
+}
+
+
+function computerChoice() {
+    return choices[Math.floor(Math.random() * choices.length)];
+};
+
+function round() {
+    const playerSelection = playerChoice();
+    const computerSelection = computerChoice();
+    const winner = verifyResults();
+}
+
+function validateInput (choice){
+    if choices.includes(choice) {
+        return true;
+    }
+};
+
+function verifyResults(playerSelection,computerSelection){
+    if(computerSelection === playerSelection){
+        return "Tie";
+    } else if(computerSelection === "Rock" && playerSelection === "Paper") ||
+        (computerSelection === "Paper" && playerSelection === "Scissors") ||
+        (computerSelection === "Scissors" && playerSelection === "Rock") { 
+            return "Player Wins!";
+    } else if (computerSelection === "Paper" && playerSelection === "Rock") ||
+        (computerSelection === "Scissors" && playerSelection === "Paper") ||
+        (computerSelection === "Rock" && playerSelection === "Scissors") {
+        return "Computer Wins!"
+    } else {
+        return "Error"
+    }
+};
+
+game();
+
+
+
+
