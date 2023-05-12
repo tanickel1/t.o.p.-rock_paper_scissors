248cfbb79cde4bd6115e68531855880216c4f9f2
Thomas Nickel <tanickel1@gmail.com>
2023-05-12 14:20:15 -0400

    added new cfunctions for pushing results to DOM

--- app.js
+++ app.js
@@ -1,56 +1,59 @@
 const choices = ["rock", "paper", "scissors"];
 const winners = [];
-
-// let playerScore = 0; 
-// let computerScore = 0;
-//let playerSelection = "";
-// let computerSelection = "";
-// let  roundWinner = "";
  
+function resetGame(){
+
+};
+
 function game() {
-    for (let i = 1; i <= 5; i++) {
-        playRound(i);
-    }
-    logWins();
+    let imgs = document.querySelectorAll('img')
+    imgs.forEach((img) => 
+    img.addEventListener((click), () => {
+        if(img.id){
+            playRound(img.id)
+        }
+    })) 
+
+};
+
+function checkWins() {
+    const pWinCount = winners.filter((item) => item == "player").length;
+    const cWinCount = winners.filter((item) => item == "computer").length;
+    return Math.max(pWinCount, cWinCount)
+
 }
 
-function playerChoice () {
-    let playerSelection = prompt("Lets play a game. Rock, Paper, or Scissors?");
-    while (playerSelection == null) {
-        playerSelection = prompt("Lets play a game. Rock, Paper, or Scissors?");
-    }
-    playerSelection = playerSelection.toLowerCase();
-    let check = validateInput(playerSelection);
-    while (check == false){
-        prompt(
-            "Please select a valid choice. Spelling needs to be correct."
-            );
-        while (playerSelection == null) {
-            prompt("Lets play a game. Rock, Paper, or Scissors?");
-        }
-    playerSelection = playerSelection.toLowerCase();
-    check = validateInput(playerSelection);
-    }
+function tallyWins() {
+    const pWinCount = winners.filter((item) => item == "player").length;
+    const cWinCount = winners.filter((item) => item == "computer").length;
+    const ties = winners.filter((item) => item == "tie").length;
+    document.querySelector('.playerScore').textContent = `Score: ${pWinCount}`;
+    document.querySelector('.computerScore').textContent = `Score: ${cWinCount}`;
+    document.querySelector('.ties').textContent = `Score: ${ties}`;
+}
+    
+
+function playerChoice (playerChoice) {
+    const computerChoice = computerSelect();
+    const winner = verifyResults(playerChoice, computerChoice)
   return playerSelection; 
 }
 
-
+//update the dom with computer selection
 function computerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 };
 
 function playRound(round) {
+    if (wins >= 5){
+        return
+    }
     const playerSelection = playerChoice();
     const computerSelection = computerChoice();
     const winner = verifyResults(computerSelection, playerSelection);
-    console.log(winner); 
-    winners.push(winner);
     logRound(playerSelection, computerSelection, winner, round)
 }
 
-function validateInput (choice){
-     return choices.includes(choice);
-};
 
 function verifyResults(playerSelection,computerSelection){
     if(computerSelection === playerSelection){
@@ -76,22 +79,13 @@ function logWins(){
     let playerWins = winners.filter((item) => item == "Player Wins!").length;
     let comptuerWins = winners.filter((item) => item == "Comptuer Wins!").length;
     let ties = winners.filter((item) => item == "Tie").length;
-    console.log("Results:");
-    console.log("Player Wins:", playerWins);
-    console.log("Computer Wins:", comptuerWins);
-    console.log("Ties:", ties);
 }
 
 function logRound(playerChoice, computerChoice, winner ,round) {
-    console.log("Round:", round);
-    console.log("Player Chose:", playerChoice);
-    console.log("Computer Chose:", computerChoice);
-    console.log(winner, "won the round!")
-    console.log("-----------------------");
 
 }
 
-
+game();
 
 
 
